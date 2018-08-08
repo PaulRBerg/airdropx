7eaa86351cda1713b5d40a50e5dae6663164f701
Paul Berg <hello@paulrberg.com>
2018-08-08 17:32:58 +0100

    Removed checker require

--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,8 @@ const firebase = require("firebase-admin");
 firebase.initializeApp(functions.config().firebase);
 firebase.firestore().settings( { timestampsInSnapshots: true });
 
-const checker = require("./checker");
 const users = require("./users");
 
 module.exports = {
-	checker: checker,
 	users: users
 };
